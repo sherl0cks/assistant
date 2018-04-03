fffbf7a6da44f56c8c06c601130dffe96d4145a2
Guy Bianco IV <gjbiancoiv@gmail.com>
2018-04-03 15:52:02 -0400

    naive example (non-woring code)

--- assistant-domain/src/test/java/io/openinnovationlabs/assistant/domain/objective/ObjectiveTest.java
+++ assistant-domain/src/test/java/io/openinnovationlabs/assistant/domain/objective/ObjectiveTest.java
@@ -1,18 +1,30 @@
 package io.openinnovationlabs.assistant.domain.objective;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class ObjectiveTest {
 
     // code samples if you need https://github.com/VaughnVernon/IDDD_Samples
     @Test
-    public void shouldSelectAnObjectiveFromAList(){
+    public void shouldReturnListOfConstraintsWhenObjectiveSelected(){
 
         // given a list of objectives
+        ArrayList<Objective> objectives = new ArrayList<Objective>();
 
         // when a user selects the "Design an Application" Objective
         // i.e. creates a command to select the objective
+        SelectObjectiveCommand selectObjectiveCommand = new SelectObjectiveCommand();
+        // TODO trigger command
 
         // then publish an event that the objective was successfully selected
+        // and return a list of contraints (relevant tot hat objective)
+        ObjectiveSelectedEvent objectiveSelectedEvent = /* TODO get event */;
+//        Assert.assertNotNull(objectiveSelectedEvent.getObjectiveId());
+        // TODO back data from mock repository
+        Objective selectedObjective = ObjectiveRepository.getObjectiveById(objectiveSelectedEvent.getObjectiveId());
+        Assert.assertEquals("Design an Application", selectedObjective.getName());
     }
 }
